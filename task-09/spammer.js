54727ebdf9f41b3f500f1cf1adc23e18d0efff2d
Silentnight20 <70523409+Silentnight20@users.noreply.github.com>
2020-11-30 22:21:56 +0530

    Add files via upload

---
+++ task-09/spammer.js
@@ -0,0 +1,14 @@
+var message = "I am spamming you Lol.";  
+var interval = 1  ; 
+var count = 1000 ; 
+var notifyInterval = 1 ; 
+var i = 0 ;
+var timer = setInterval(function(){
+	document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = message;
+	$('.im_submit').trigger('mousedown');	
+	i++;
+	if( i  == count )
+	clearInterval(timer);
+	if( i % notifyInterval == 0)
+	console.log(i + ' MESSAGES SENT');
+} , interval * 1000 ) ;
